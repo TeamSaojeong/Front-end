6225ef31429ae0c45f7ab4dbc4ef812209b9cb4e
Seongwon20 <98306482+Seongwon20@users.noreply.github.com>
2025-08-09 21:46:29 +0900

    feat: LoginPage에서 나중 서버 통신을 위한 handleLogin()선언

--- carpark/src/App.jsx
+++ carpark/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Routes>
-        {/* <Route path='/' element={<Home /> } /> */}
+        {/* <Route path='/' element={<Home /> } /> */} 
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
 
--- carpark/src/pages/LoginPage.jsx
+++ carpark/src/pages/LoginPage.jsx
@@ -8,6 +8,10 @@ import password_icon from '../Assets/password.png';
 const LoginPage = () => {
   const navigate = useNavigate();
 
+  const handleLogin = () => {
+    //여기에 나중에 axios든 뭐든 써서 로그인 정보 받아오게 할 예정
+  }
+
   return (
     <div className='container'>
       <div className="header">
@@ -26,7 +30,7 @@ const LoginPage = () => {
       </div>
       <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
       <div className="submit-container">
-        <div className="submit" onClick={() => {/* 로그인 로직 */}}>Login</div>
+        <div className="submit" onClick={handleLogin}>Login</div>
         <div className="submit gray" onClick={() => navigate('/signupage')}>Go to Sign Up</div>
       </div>
     </div>
