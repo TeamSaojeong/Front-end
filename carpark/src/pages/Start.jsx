56a73526997af779d8afa29aa19318dc98787b33
Seongwon20 <98306482+Seongwon20@users.noreply.github.com>
2025-08-12 01:35:21 +0900

    Feat: 시작화면:start.jsx,css 추가 and 로그인,회원가입 버튼에 hover 적용

---
+++ carpark/src/pages/Start.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../Styles/Start.css";
+
+import mainpic from "../Assets/main_logo_text.png";
+import car_icon from "../Assets/car.png";
+
+const Start = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <div className="header">
+        <img src={mainpic} alt="메인 로고" />
+      </div>
+
+      <div className="car-section">
+        <img src={car_icon} alt="자동차 아이콘" />
+      </div>
+
+      <div className="Nowstart" onClick={() => navigate("/login")}>
+        바로 시작하기
+      </div>
+
+      <div className="notyet-customer" onClick={() => navigate("/signup")}>
+        아직 회원이 아니신가요? <span>회원가입</span>
+      </div>
+    </div>
+  );
+};
+export default Start;
