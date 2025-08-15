ad22ff92f7f5a64a4a25e0bcd9b746eea2b29de9
Seongwon20 <98306482+Seongwon20@users.noreply.github.com>
2025-08-16 00:28:53 +0900

    feat: 결제완료 페이지 구현 완료

--- carpark/src/pages/Pay/PayComplete.jsx
+++ carpark/src/pages/Pay/PayComplete.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../Styles/Pay/PayComplete.css";
 
 import arrow from "../../Assets/arrow.png";
-import backcar_icon from "../../Assets/paycomplete.svg";
+import car_icon from "../../Assets/paycomplete.svg";
+import clock_icon from "../../Assets/clock.svg";
 
-const ParkingEnd = () => {
+
+const PayComplete = () => {
   const navigate = useNavigate();
 
   return (
@@ -16,33 +18,37 @@ const ParkingEnd = () => {
         className="back-arrow"
         onClick={() => navigate(-1)}
       />
-
       <div className="paycomplete-header">
         <div className="paycomplete-text">
-          주차 이용 종료
+          결제완료
           <br />
-          <p>
-            주차 이용 시간 종료되었습니다!
+          <p>정상적으로 결제가 완료되었습니다.
             <br />
-            다음 이용자를 위해 출차를 부탁드립니다.
+            자동으로 화면이 넘어갈 예정입니다.
           </p>
         </div>
       </div>
 
-      <div className="car-section">
-        <img src={backcar_icon} alt="자동차 아이콘" />
+      <div className="paycomplete-time-box">
+        <div className="paycomplete-time-inner">
+          <img src={clock_icon} alt="시계 아이콘" className="clock-icon" />
+          <span className="paycomplete-time-text">00:00 ~ 00:00 (3시간 20분)</span>
+        </div>
+      </div>
+
+      <div className="paycomplete-car-section">
+        <img src={car_icon} alt="자동차 아이콘" />
       </div>
 
       <div className="paycomplete-button-section">
-        <button
-          className="paycomplete-outcomplete"
-          onClick={() => navigate("/outsoon")}
-        >
-          출차 완료
-        </button>
+          <button
+            className="paycomplete-outsoon"
+            onClick={() => navigate("/login")}
+          >
+            주차를 해주세요!
+          </button>
+        </div>
       </div>
-    </div>
   );
 };
-
-export default ParkingEnd;
+export default PayComplete;
