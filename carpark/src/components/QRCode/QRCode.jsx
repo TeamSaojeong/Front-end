e11689be643f6c9f987c9546a7d7d44c67bdbb6d
Seongwon20 <98306482+Seongwon20@users.noreply.github.com>
2025-08-21 00:50:54 +0900

    feat: qr 삭제

--- carpark/src/components/QRCode/QRCode.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import {QRCodeSVG} from 'qrcode.react';
-
-ReactDOM.render(
-    <QRCodeSVG value="https://reactjs.org/" />,
-    document.getElementById('mountNode')
-);
